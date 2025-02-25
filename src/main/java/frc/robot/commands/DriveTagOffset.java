ed42c90500cb7a1f43f34011b9609f72cdb2b9f1
Mahdi <mahdi.alhassani2007@gmail.com>
2025-02-25 09:15:39 -0800

    Add Heading correction to DTO (NOT IMPLEMENTED UNTIL OTHER IS TESTED)

--- src/main/java/frc/robot/commands/DriveTagOffset.java
+++ src/main/java/frc/robot/commands/DriveTagOffset.java
@@ -4,8 +4,11 @@ import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.VisionConstants;
 import frc.robot.Telemetry;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -30,6 +33,8 @@ public class DriveTagOffset extends Command{
     
     private final PIDController PIDx;
     private final PIDController PIDy;
+    private final PIDController PIDh;
+
 
     public DriveTagOffset(CommandSwerveDrivetrain drivetrain, VisionSubsystem vision, Telemetry logger, 
                           int cameraIndex, double sideOffset, double forwardOffset){
@@ -47,6 +52,9 @@ public class DriveTagOffset extends Command{
         PIDy = new PIDController(6, 0.0, 0.0);
         PIDy.setSetpoint(0.0);
 
+        PIDh = new PIDController(3, 0.0, 0.0);
+        PIDh.setSetpoint(0.0);
+
         addRequirements(drivetrain, vision);
     }
 
@@ -57,16 +65,46 @@ public class DriveTagOffset extends Command{
 
         PIDx.reset();
         PIDy.reset();
+        PIDh.reset();
     }
 
     @Override
     public void execute() {
+        // ROTATION
+        var maybeTagId = vision.getPrimaryTagId(cameraIndex);
+        if (maybeTagId.isEmpty()) {
+            // If no tag is detected, cancel the command.
+            cancel();
+            return;
+        }
+
+        int tagId = maybeTagId.get();
+
+        // Look up the known pose of that tag from the field layout.
+        var maybeTagPose = VisionConstants.aprilTagLayout.getTagPose(tagId).map(p3d -> p3d.toPose2d());
+        if (maybeTagPose.isEmpty()) {
+            cancel();
+            return;
+        }
+
+        Pose2d tagPose = maybeTagPose.get();
+
+        double targetHeadingRad = tagPose.rotateBy(Rotation2d.fromRadians(Math.PI)).getRotation().getRadians();
+        double currentHeadingRad = logger.getHeading().getRadians();
+
+        double headingErorr = targetHeadingRad - currentHeadingRad;
+
+        // Normalize error to the range [-180, 180] degrees.
+        headingErorr = ((headingErorr + Math.PI) % (2*Math.PI)) - Math.PI;
+
+        // X & Y
         TX = vision.getTX(cameraIndex);
         TDISTANCE = Math.sqrt(Math.pow(vision.getDistance(cameraIndex), 2) - Math.pow(TX, 2));
 
         // flip because Y is the side to side of robot; X is forward back of robot
         double velY = PIDx.calculate(sideOffset - TX);
         double velX = PIDy.calculate(TDISTANCE - forwardOffset);
+        double rotRate = PIDh.calculate(headingErorr);                          // NOT SETTING ROTATIONAL RATE (WILL TEST AFTER WORKING)
 
         drivetrain.setControl(
             new SwerveRequest.RobotCentric()
